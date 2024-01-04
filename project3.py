6f1423f5fe296be5a0f4b66c353503851201dae9
snehavr799 <153365366+snehavr799@users.noreply.github.com>
2024-01-04 14:47:14 +0530

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+def flower():
+    for i in range(300):
+        circle(190-i,90)
+        left(90)
+        circle(190-i,90)
+        left(18)
+        speed(20)
+
+flower()
+mainloop()        
